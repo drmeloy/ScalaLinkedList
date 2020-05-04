e9d1f2201a383d90bd3d632c23b6c9347a91fbb2
drmeloy <drmeloy@gmail.com>
2020-05-04 11:06:35 -0700

    Implemented recursive foldLeft and contains methods on MyList

--- src/main/scala/MyList.scala
+++ src/main/scala/MyList.scala
@@ -11,12 +11,26 @@ sealed trait MyList[T] {
       case node: Node[Double] => BigDecimal(node.head) + node.tail.sum
       case _ => throw new Exception("Can only sum numbers")
     }
+
+  def foldLeft[U](identity: U)(op: (U, T) => U): U =
+    this match {
+      case Node(h, t) => val accumulator = op(identity, h)
+        t.foldLeft(accumulator)(op)
+      case End() => identity
+    }
+
+  def contains(item: T): Boolean =
+    this match {
+      case Node(h, t) => h == item || t.contains(item)
+      case End() => false
+    }
 }
 
 case class Node[T](head: T, tail: MyList[T]) extends MyList[T]
 case class End[T]() extends MyList[T]
 
 object Main extends App {
-  val list: MyList[String] = Node("1", Node("Hi", Node("3", End())))
-  println(list.length)
+  val list: MyList[Int] = Node(1, Node(2, Node(3, End())))
+  val names: MyList[String] = Node("Jake", Node("John", Node("Mike", End())))
+  println(names.contains("Mike"))
 }
