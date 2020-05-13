f5a57e6ba935eb1d0a6a627b8910648d8c940b16
drmeloy <drmeloy@gmail.com>
2020-05-13 12:35:56 -0700

    Implemented map, flatmap, ++, and find

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,6 @@
+//object Main extends App {
+//  val list: MyList[Int] = Node(1, Node(2, Node(3, End())))
+//  val names: MyList[String] = Node("Jake", Node("John", Node("Mike", End())))
+//  println(names.contains("Mike"))
+//  println(names.find(name => name.startsWith("M")))
+//}
--- src/main/scala/MyList.scala
+++ src/main/scala/MyList.scala
@@ -1,3 +1,17 @@
+object Main extends App {
+  val list1: MyList[Int] = Node(1, Node(2, Node(3, End())))
+  val list2: MyList[Int] = Node(4, Node(5, Node(6, End())))
+  val names: MyList[String] = Node("Jake", Node("John", Node("Mike", End())))
+  println(names.contains("Mike"))
+  println(names.find(name => name.startsWith("M")))
+  println(names.filter(name => name.startsWith("J")))
+  println(names.filter(name => name.startsWith("T")))
+  println(names.map(name => name.toUpperCase))
+  println(names.lastOption)
+  println(End().lastOption)
+  println(list1 ++ list2)
+}
+
 sealed trait MyList[T] {
   def length: Int =
     this match {
@@ -24,13 +38,76 @@ sealed trait MyList[T] {
       case Node(h, t) => h == item || t.contains(item)
       case End() => false
     }
+
+  def find(func: T => Boolean): Option[T] =
+    this match {
+      case Node(h, t) =>
+        if(func(h)) Some(h)
+        else t.find(func)
+      case End() =>
+        None
+    }
+
+  def filter(func: T => Boolean): MyList[T] =
+    this match {
+      case Node(h, t) =>
+        if(func(h)) Node(h, t.filter(func))
+        else t.filter(func)
+      case End() =>
+        End()
+    }
+
+  def map[U](func: T => U): MyList[U] =
+    this match {
+      case Node(h, t) =>
+        Node(func(h), t.map(func))
+      case End() =>
+        End()
+    }
+
+  def lastOption: Option[T] =
+    this match {
+      case Node(h, t) =>
+        t match {
+          case Node(_, _) =>
+            t.lastOption
+          case End() =>
+            Some(h)
+        }
+      case End() =>
+        None
+    }
+
+  def ++(that: MyList[T]): MyList[T] = {
+    this match {
+      case Node(thisHead, thisTail) =>
+        that match {
+          case Node(_, _) =>
+            val newTail =
+              thisTail match {
+                case Node(_, _) =>
+                  thisTail ++ that
+                case End() =>
+                  that
+              }
+            val concatenatedLists = Node(thisHead, newTail)
+            concatenatedLists
+          case End() =>
+            this
+        }
+      case End() =>
+        that
+    }
+  }
+
+  def flatmap[U](func: T => MyList[U]): MyList[U] =
+    this match {
+      case Node(h, t) =>
+        func(h) ++ t.flatmap(func)
+      case End() =>
+        End()
+    }
 }
 
 case class Node[T](head: T, tail: MyList[T]) extends MyList[T]
 case class End[T]() extends MyList[T]
-
-object Main extends App {
-  val list: MyList[Int] = Node(1, Node(2, Node(3, End())))
-  val names: MyList[String] = Node("Jake", Node("John", Node("Mike", End())))
-  println(names.contains("Mike"))
-}
